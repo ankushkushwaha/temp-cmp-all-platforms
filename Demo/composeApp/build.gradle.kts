f5b2a143704d28b734e7c01ffc678598c06befa7
ankushkushwaha <ankush7377@gmail.com>
2025-01-15 15:46:56 +0530

    Add remaining dependencies in gradle

--- Demo/composeApp/build.gradle.kts
+++ Demo/composeApp/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinxSerialization)
 }
 
 kotlin {
@@ -55,16 +56,49 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+
+            implementation(libs.ktor.client.okhttp)
+
+            implementation(libs.androidx.material)
+
+            implementation(libs.androidx.material3)
+
+            implementation("com.google.android.gms:play-services-location:18.0.0") // Location services dependency for Android
+
+            implementation("androidx.compose.ui:ui:1.5.0") // Jetpack Compose UI components
+            api(libs.moko.permissions)
+            api(libs.moko.permissions.compose)
         }
+
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            api(libs.moko.permissions)
+            api(libs.moko.permissions.compose)
+        }
+
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
+            implementation(compose.material3)
             implementation(compose.material)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.coil.compose)
+            implementation(libs.coil.network.ktor)
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.navigation.compose)
+//            api(libs.moko.permissions)
+//            api(libs.moko.permissions.compose)
+//            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
         }
     }
 }
@@ -97,6 +131,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.volley)
     debugImplementation(compose.uiTooling)
 }
-
